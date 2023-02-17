365023ae2ce177e66ee3676adb9d6ac99a27e55e
Deus210 <103108069+Deus210@users.noreply.github.com>
2023-02-17 15:39:57 +0300

    Add files via upload

---
+++ chupahin CAR/chupahin CAR/ChangeCarCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ChangeCarCollectionViewCell.swift
+//  chupahin CAR
+//
+//  Created by Чупахин Аркадий Александрович on 13/2/2023.
+//
+
+import UIKit
+
+class ChangeCarCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var carPicture: UIImageView!
+    
+}
---
+++ chupahin CAR/chupahin CAR/ResultTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  ResultTableViewCell.swift
+//  chupahin CAR
+//
+//  Created by Чупахин Аркадий Александрович on 13/2/2023.
+//
+
+import UIKit
+
+class ResultTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var namePlayer: UILabel!
+    @IBOutlet weak var gameResult: UILabel!
+    @IBOutlet weak var timeResult: UILabel!
+    
+}
